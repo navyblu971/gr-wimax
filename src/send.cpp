8b5bd093ee8ac9c31914a289fd0c03475caa20b3
navyblu971@hotmail.com <navyblu971@hotmail.com>
2017-07-23 19:16:55 +0200

    sauvegarde

--- src/send.cpp
+++ src/send.cpp
@@ -28,9 +28,87 @@ typedef struct  {
 } user ;
 user my_user ;
 #define OFDM_SIZE 512
+unsigned char gOFDMbuffer[OFDM_SIZE] ;
 int g_numberOfUsers = 23 ;
+unsigned char *data ;
+
 
 unsigned char gOfdmSymbolBuffer[OFDM_SIZE] ;
+
+
+/*
+ *
+ *
+ *
+ * get nb bytes from interface id..
+ */
+void getData2send (void * data,int nbWanted ,  int* nbReceived , int id)
+{
+
+
+
+
+}
+
+bool gAlive = true ;
+void sender_loop ()
+{
+
+	int room = std::abs(OFDM_SIZE /8 );
+	int id =0 ; //cureent interface
+	int nb ;
+	int bytePerInterface = 5 ;
+	while (gAlive)
+	{
+
+		/* tant qu'il y a de la place et tant qu'on a pas attendu trop longtemps ..*/
+		/*tant qu'il y a des interface */
+		//regarder si il y a des datas a envoyer
+		//si il y a des data, lire nb data suivant la politique
+
+
+		while  (room > 0 )
+		{
+
+			getData2send (data,bytePerInterface ,  &nb ,  id)
+			if (nb > 0)
+			{
+
+				if (nb < BYTE_PER_USER)
+				addDataToOfdm(  id, data, nb)
+				else
+					addDataToOfdm(  id, data, BYTE_PER_USER)
+				room-=nb ;
+
+			}
+
+
+			id++ ;
+
+		}
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
+
+
+void addDataToOfdm(  int id, unsigned  data, int nb)
+{
+
+	gOFDMbuffer[id] = gOFDMbuffer[id-1] + nb ;
+
+
+}
 
 
 bool send ( user u[g_numberOfUsers ] )
@@ -41,12 +119,14 @@ bool send ( user u[g_numberOfUsers ] )
 	int headerSize =0 ;
 
 	/* get header size */
-	while (i < g_numberOfUsers)
-	{
-		headerSize += u[g_numberOfUsers].ramdrive.size ;
-		i++;
+	//while (i < g_numberOfUsers)
+	//{
+	//	headerSize += u[g_numberOfUsers].ramdrive.size ;
+	//	i++;
 
-	}
+	//}
+	headerSize = g_numberOfUsers * sizeof(unsigned char) ;
+
 
 
 	/* fill header */
